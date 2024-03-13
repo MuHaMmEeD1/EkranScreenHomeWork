74e4f754e6ed1951ac30b466cca19bf67588acb4
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2024-03-13 16:20:08 +0400

    Add project files.

---
+++ ClientScreenShot/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System;
+
+namespace ClientScreenShot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        EndPoint remoutEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 27001);
+        bool Started = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+
+
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Started)
+            {
+                IsleMetod();
+                Started = false;
+            }
+
+        }
+
+        async Task IsleMetod()
+        {
+            await Task.Run(() => {
+
+                byte[] buffer = new byte[ushort.MaxValue - 30];
+
+                client.SendTo(buffer, remoutEndPoint);
+
+                while (true)
+                {
+                    buffer = new byte[ushort.MaxValue - 30];
+                    List<byte> EsasBufferList = new List<byte>();
+
+                    while (true)
+                    {
+                        int bytesReceived = client.ReceiveFrom(buffer, ref remoutEndPoint);
+                        EsasBufferList.AddRange(buffer.Take(bytesReceived));
+
+                        if (bytesReceived < buffer.Length)
+                        {
+                            break;
+                        }
+                    }
+
+                    byte[] EsasBuffer = EsasBufferList.ToArray();
+
+
+
+                    Dispatcher.Invoke(() => {
+                        BitmapImage bitmap = new BitmapImage();
+
+                        using (MemoryStream ms = new MemoryStream(EsasBuffer))
+                        {
+                            bitmap.BeginInit();
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.StreamSource = ms;
+                            bitmap.EndInit();
+                        }
+
+                        EsasImage.Source = bitmap;
+                    });
+                }
+
+            });
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ EkranScreenHomeWork/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 27001);
+
+            server.Bind(endPoint);
+
+            byte[] buffer = new byte[ushort.MaxValue - 30];
+
+            while (true)
+            {
+                EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                try
+                {
+                    server.ReceiveFrom(buffer, ref senderEndPoint);
+                }
+                catch(Exception ex) { continue; }
+                Task.Run(() =>
+                {
+                    EndPoint sEP = senderEndPoint;
+                    while (true)
+                    {
+                        try
+                        {
+                            Bitmap screenshot = CaptureScreen();
+                            byte[] imageBytes = ImageToBytes(screenshot);
+
+                            var bayts = imageBytes.Chunk(ushort.MaxValue - 30);
+
+
+                            foreach (var by in bayts)
+                            {
+                                server.SendTo(by, sEP);
+                            }
+
+                            Thread.Sleep(10);
+                        }
+                        catch(Exception ex) { Console.WriteLine(ex.Message); break; }
+                    }
+                });
+            }
+        }
+
+        static Bitmap CaptureScreen()
+        {
+            Rectangle bounds = new Rectangle(0, 0, 1920, 1080);
+            Bitmap screenshot = new Bitmap(bounds.Width, bounds.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(bounds.X, bounds.Y, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            return screenshot;
+        }
+
+        static byte[] ImageToBytes(Bitmap image)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                image.Save(ms, ImageFormat.Jpeg);
+                return ms.ToArray();
+            }
+        }
+    }
+}
